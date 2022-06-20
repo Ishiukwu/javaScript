5f1b960913b6c1dc1849747ebc3cbd61e0c6aedf
Samuel Chimezie <samuelogoegbu@gmail.com>
2022-06-20 17:19:03 +0100

    Done with that
    A JavaScript function that prints out my name, height and country on the screen

---
+++ new.js
@@ -0,0 +1,7 @@
+function write_Name(name, height, country){
+  return `My name is ${name}, I am ${height} feet tall and I am from ${country}`
+}
+
+
+
+document.write(write_Name('Samuel Ogoegbu', 5.5, 'Nigeria'))
